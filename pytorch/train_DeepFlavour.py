8102063ff2e61440d5180ebffdae5315005a2739
Joshuha <jthomasw@cern.ch>
2022-05-18 17:23:43 +0200

    bug fix in return type from training

--- pytorch/train_DeepFlavour.py
+++ pytorch/train_DeepFlavour.py
@@ -30,5 +30,5 @@ train=training_base(model = model, criterion = criterion, optimizer = optimizer,
 
 train.train_data.maxFilesOpen=1
 
-model,history = train.trainModel(nepochs=num_epochs+lr_epochs, 
-                                 batchsize=4000)
+#model,history = train.trainModel(nepochs=num_epochs+lr_epochs,batchsize=4000)
+history = train.trainModel(nepochs=num_epochs+lr_epochs,batchsize=4000)
